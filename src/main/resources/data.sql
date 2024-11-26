11c595810eb01ad80ae342486a323e771b5a0c9c
kimoueric <kimoueric04@gmail.com>
2024-11-26 00:13:21 +0000

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+DELETE FROM customers;
+INSERT INTO customers (email) VALUES ('kimoueric@gmail.com');
+INSERT INTO customers (email) VALUES ('acceuil@gmail.com');
+INSERT INTO customers (email) VALUES ('yao@gmail.com');
+INSERT INTO customers (email) VALUES ('berte@gmail.com');
+INSERT INTO customers (email) VALUES ('junior@gmail.com');
---
+++ src/test/java/hifadih/positions/PositionsApplicationTests.java
@@ -0,0 +1,13 @@
+package hifadih.positions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
